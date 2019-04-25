a19ebbbf433897294c1148309631bc715dd7c68b
Lukas Frösslund <frosslundl@gmail.com>
2019-04-25 14:23:48 -0300

    added edit and update routes for comments

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -3,7 +3,7 @@ var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 
-//Comments New
+//NEW -
 router.get("/new", isLoggedIn, function(req, res) {
   // Find campground by id
   Campground.findById(req.params.id, function(err, campground) {
@@ -15,7 +15,7 @@ router.get("/new", isLoggedIn, function(req, res) {
   });
 });
 
-//Comments Create
+//CREATE - 
 router.post("/", isLoggedIn, function(req ,res) {
   //look up campground using id
   Campground.findById(req.params.id, function(err, campground) {
@@ -43,6 +43,29 @@ router.post("/", isLoggedIn, function(req ,res) {
      }
   });
 }); 
+
+//EDIT - 
+router.get("/:comment_id/edit", function(req, res) {
+   Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if(err) {
+         res.redirect("back");
+      } else {
+         res.render("comments/edit", {campground_id : req.params.id, comment: foundComment});
+      }
+   });
+});
+
+//UPDATE -
+router.put("/:comment_id", function(req, res) {
+   //find and update the correct comment
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+      if(err) {
+         res.redirect("back");
+      } else {
+         res.redirect("/campgrounds/" + req.params.id);
+      }
+   });
+});
 
 //middleware
 function isLoggedIn(req, res, next){
