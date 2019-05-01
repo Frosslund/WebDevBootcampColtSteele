fa04f1954bb3a86a1d20dd529640890612d7a1af
Lukas Frösslund <frosslundl@gmail.com>
2019-05-01 14:22:48 -0400

    added flash messages

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,6 +5,7 @@ var express          = require("express"),
     Campground       = require("./models/campground"),
     Comment          = require("./models/comment"),
     passport         = require("passport"),
+    flash            = require("connect-flash"),
     LocalStrategy    = require("passport-local"),
     User             = require("./models/user"),
     methodOverride   = require("method-override");
@@ -21,6 +22,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB(); // seed the database
 
 // Passport Configuration
@@ -38,6 +40,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
    next();
 });
 
